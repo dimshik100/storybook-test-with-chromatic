f5d5640e650a3e62de55f202f7ec749e48323099
Dima Vishnevetsky <dimshik100@gmail.com>
2022-12-30 13:35:12 +0200

    remove playwright for now

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,3 @@
-// const { setConfig } = require("storybook-addon-playwright/configs");
-// const playwright = require("playwright");
-
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -10,9 +7,6 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    // '@storybook/addon-knobs/register',
-    // 'storybook-addon-playwright/preset',
-    // 'storybook-addon-playwright/register',
   ],
   "framework": "@storybook/vue3",
   "core": {
@@ -22,21 +16,3 @@ module.exports = {
     "storyStoreV7": true
   }
 }
-
-// (async () => {
-//   let browser = {
-//     chromium: await playwright['chromium'].launch(),
-//     firefox: await playwright['firefox'].launch(),
-//     webkit: await playwright['webkit'].launch(),
-//   };
-//   setConfig({
-//     storybookEndpoint: `http://localhost:6006/`,
-//     getPage: async (browserType, options) => {
-//       const page = await browser[browserType].newPage(options);
-//       return page;
-//     },
-//     afterScreenshot: async (page) => {
-//       await page.close();
-//     },
-//   });
-// })();
